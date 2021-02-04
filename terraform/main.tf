d56c200c4a3e2da2a58a1321e86c479d7566ad6a
Pedro Nakano Tramontin <pedro.tramontin@n26.com>
2021-02-04 21:24:53 +0100

    Changing my-script.py to return a name inside the payload And changing main.tf to have a different name

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,20 @@ terraform {
 }
 
 provider "multiverse" {}
+
+resource "multiverse_custom_resource" "custom-resource" {
+  executor = "python3"
+  script = "my-script.py"
+  id_key = "id"
+  name = "test"
+  data = <<JSON
+{
+  "name": "test-terraform-test",
+  "mlb_id": "lb-124",
+  "mlb_deployment_id": "dp-123",
+  "mlb_listener_ids": ["ls-124", "ls-456"],
+  "test_group_callback_fqdn": "test.fqdn.com",
+  "control_group_callback_fqdn": "control.fqdn.com"
+}
+JSON
+}
